4c505841da329db3593d77f32d1bd6b3a5d35a31
Ava Pek <code@exodrifter.space>
2024-05-25 05:00:56 -0500

    Initial Kettle commit

--- main.ts
+++ main.ts
@@ -1,81 +1,65 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Notice, Plugin, PluginSettingTab, Setting, normalizePath } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface KettleSettings {
+	location: string;
+	format: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: KettleSettings = {
+	location: '',
+	format: 'YYYYMMDD_kkmmss',
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class Kettle extends Plugin {
+	settings: KettleSettings;
+
+	async createUniqueNote(): Promise<void> {
+		const name = moment().utc().format(this.settings.format);
+		const path = normalizePath(`/${this.settings.location}/${name}.md`);
+		console.log(path)
+
+		try {
+			console.log(this.app)
+			const fileExists = await this.app.vault.adapter.exists(path);
+			if (fileExists) {
+				throw new Error(`${path} already exists!`);
+			}
+			const file = await this.app.vault.create(path, '');
+			// Create the file and open it in the active leaf
+			let leaf = this.app.workspace.getLeaf(false);
+			if (this.mode === "new-pane") {
+				leaf = this.app.workspace.splitLeafOrActive();
+			} else if (this.mode === "new-tab") {
+				leaf = this.app.workspace.getLeaf(true);
+			} else if (!leaf) {
+				// default for active pane
+				leaf = this.app.workspace.getLeaf(true);
+			}
+			await leaf.openFile(file);
+		} catch (error) {
+			new Notice(error.toString());
+		}
+	}
 
 	async onload() {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		this.addRibbonIcon('file-stack', 'Create new unique note', async (evt: MouseEvent) => {
+			await this.createUniqueNote()
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+			id: 'create-new-unique-note',
+			name: 'Create new unique note',
+			callback: async () => {
+				await this.createUniqueNote()
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new KettleSettingTab(this.app, this));
 	}
 
 	onunload() {
@@ -91,26 +75,10 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class KettleSettingTab extends PluginSettingTab {
+	plugin: Kettle;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: Kettle) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -121,14 +89,31 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('New file location')
+			.setDesc('The folder path to create the new unique note.')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('')
+				.setValue(this.plugin.settings.location)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.location = value;
 					await this.plugin.saveSettings();
 				}));
+
+		const setting = new Setting(containerEl)
+		setting.setName('Unique prefix format')
+			.setDesc(this.formatExample())
+			.addText(text => text
+				.setPlaceholder('YYYYMMDD_kkmmss')
+				.setValue(this.plugin.settings.format)
+				.onChange(async (value) => {
+					this.plugin.settings.format = value;
+					await this.plugin.saveSettings();
+					setting.setDesc(this.formatExample())
+				}));
+	}
+
+	formatExample(): String {
+		const example = moment().utc().format(this.plugin.settings.format)
+		return `moment.js format string. Currently: ${example}`
 	}
 }
